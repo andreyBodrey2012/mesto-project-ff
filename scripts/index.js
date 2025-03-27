8d64844ba4e3e101e2b2150a377a09f283974898
Andrey Zapozdaev <andrejzapozdaev@gmail.com>
2025-03-27 12:03:21 +0300

    updated

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const cardPlace = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(params, deleteCard) {
+    const cloneCard = cardTemplate.querySelector('.card').cloneNode(true);
+    cloneCard.querySelector('.card__title').innerText = params.name;
+    const cardImage = cloneCard.querySelector('.card__image');
+    cardImage.src = params.link;
+    cardImage.alt = params.name;
+    cloneCard.querySelector('.card__delete-button').addEventListener('click', () => { deleteCard(cloneCard) });
+
+    cardPlace.append(cloneCard);
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(element) {
+    if (element && typeof element.remove === "function") {
+        element.remove();
+    }
+}
 
 // @todo: Вывести карточки на страницу
+document.querySelector(".footer__copyright").innerText = `© ${(new Date).getFullYear()} Mesto Russia`;
+
+initialCards.forEach((item)=>{
+    createCard(item, deleteCard);
+});
