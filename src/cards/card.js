d2635cbf3325ee854dec46664347951a13881793
Andrey Zapozdaev <andrejzapozdaev@gmail.com>
2025-04-24 20:41:05 +0300

    Again is diggest updated

--- src/cards/card.js
+++ src/cards/card.js
@@ -1,89 +1,26 @@
-import { initialCards } from "./constans";
-
-let defautParams = {
-  cardImageSelector: ".card__image",
-  popupTypeImageSelector: ".popup_type_image",
-  cardTemplateSelector: "#card-template",
-  placesListSelector: ".places__list",
-  cardSelector: ".card",
-  cardTitleSelector: ".card__title",
-  cardDeleteButtonSelector: ".card__delete-button",
-  cardLikeButtonSelector: ".card__like-button",
-  popupImageSelector: ".popup__image",
-  popupCaptionSelector: ".popup__caption",
-  cardLikeButtonIsActiveSelector: "card__like-button_is-active",
-};
-
-export function initCards(params={}) {
-  defautParams = {
-    ...defautParams, ...params
-  };
-  const cards = initialCards.map((item) =>
-    createCard({ ...item, deleteCard, likeCard, imageCard }),
-  );
-  cardsPlace.append(...cards);
-}
-
-export function addNewCard(name, link, openWindow) {
-  const newCard = createCard({ name, link, deleteCard, likeCard, imageCard });
-  cardsPlace.prepend(newCard);
-  newCard
-    .querySelector(defautParams.cardImageSelector)
-    .addEventListener(
-      "click",
-      openWindow(document.querySelector(defautParams.popupTypeImageSelector)),
-    );
-}
-
-// @todo: Темплейт карточки
-const cardTemplate = document.querySelector(defautParams.cardTemplateSelector).content;
-
-// @todo: DOM узлы
-export const cardsPlace = document.querySelector(defautParams.placesListSelector);
-
 // @todo: Функция создания карточки
-export function createCard(params) {
-  const cloneCard = cardTemplate.querySelector(defautParams.cardSelector).cloneNode(true);
-  cloneCard.querySelector(defautParams.cardTitleSelector).innerText = params.name;
-  const cardImage = cloneCard.querySelector(defautParams.cardImageSelector);
+export function createCard(data, { template, titleSelecor, imageSelector }) {
+  const card = template.cloneNode(true);
+  card.querySelector(titleSelecor).innerText = data.name;
+  const cardImage = card.querySelector(imageSelector);
 
-  cardImage.src = params.link;
-  cardImage.alt = params.name;
-  cloneCard
-    .querySelector(defautParams.cardDeleteButtonSelector)
-    .addEventListener("click", () => {
-      params.deleteCard(cloneCard);
-    });
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
 
-  cloneCard
-    .querySelector(defautParams.cardLikeButtonSelector)
-    .addEventListener("click", () => {
-      params.likeCard(cloneCard.querySelector(defautParams.cardLikeButtonSelector));
-    });
-
-  cardImage.addEventListener("click", () => {
-    params.imageCard(params);
-  });
-
-  return cloneCard;
+  return card;
 }
 
 // @todo: Функция удаления карточки
-function deleteCard(element) {
+export function deleteCard(element) {
   if (element && typeof element.remove === "function") {
     element.remove();
   }
 }
 
-function likeCard(element) {
-  if (element.classList.contains(defautParams.cardLikeButtonIsActiveSelector)) {
-    element.classList.remove(defautParams.cardLikeButtonIsActiveSelector);
+export function likeCard(element, activeClass = "card__like-button_is-active") {
+  if (element.classList.contains(activeClass)) {
+    element.classList.remove(activeClass);
   } else {
-    element.classList.add(defautParams.cardLikeButtonIsActiveSelector);
+    element.classList.add(activeClass);
   }
-}
-
-function imageCard(element) {
-  document.querySelector(defautParams.popupImageSelector).src = element.link;
-  document.querySelector(defautParams.popupCaptionSelector).textContent = element.name;
 }
--- src/index.js
+++ src/index.js
@@ -1,36 +1,185 @@
-import { initCards } from "./cards/card";
-import { initPopup } from "./popup/modal";
+import { createCard, deleteCard, likeCard } from "./cards/card";
+import { initialCards } from "./cards/cards";
+import { openWindow, handlerClosePopup, closePopup } from "./popup/modal";
 import "./css/index.css";
 
-export let defautParams = {
-  profileAddButtonSelector: ".profile__add-button",
-  popupTypeNewCardSelector: ".popup_type_new-card",
-  profileEditButtonSelector: ".profile__edit-button",
-  popupTypeEditSelector: ".popup_type_edit",
-  popupCloseSelector: ".popup__close",
-  popupIsOpenedSelector: ".popup_is-opened",
-  popupFormEditProfileSelector: ".popup__form[name='edit-profile']",
-  popupFormNewPlaceSelector: ".popup__form[name='new-place']",
-  popupInputTypeNameSelector: ".popup__input_type_name",
-  popupInputTypeDescriptionSelector: ".popup__input_type_description",
-  profileTitleSelector: ".profile__title",
-  profileDescriptionSelector: ".profile__description",
-  popupInputTypeCardNameSelector: ".popup__input_type_card-name",
-  popupInputTypeUrlSelector: ".popup__input_type_url",
-  popupSelector: ".popup",
+const elementSelectors = {
+  // cards
+  cardImage: ".card__image",
+  popupTypeImage: ".popup_type_image",
+  cardTemplate: "#card-template",
+  placesList: ".places__list",
+  card: ".card",
+  cardTitle: ".card__title",
+  cardDeleteButton: ".card__delete-button",
+  cardLikeButton: ".card__like-button",
+  popupImage: ".popup__image",
+  popupCaption: ".popup__caption",
+  cardLikeButtonIsActive: "card__like-button_is-active",
+  // modals
+  profileAddButton: ".profile__add-button",
+  popupTypeNewCard: ".popup_type_new-card",
+  profileEditButton: ".profile__edit-button",
+  popupTypeEdit: ".popup_type_edit",
+  popupClose: ".popup__close",
+  popupTypeImage: ".popup_type_image",
+  popupIsOpened: ".popup_is-opened",
+  cardImage: ".card__image",
+  popupFormEditProfile: ".popup__form[name='edit-profile']",
+  popupFormNewPlace: ".popup__form[name='new-place']",
+  popupInputTypeName: ".popup__input_type_name",
+  popupInputTypeDescription: ".popup__input_type_description",
+  profileTitle: ".profile__title",
+  profileDescription: ".profile__description",
+  popupInputTypeCardName: ".popup__input_type_card-name",
+  popupInputTypeUrl: ".popup__input_type_url",
+  popup: ".popup",
   popupOpened: "popup_is-opened",
-  popupIsAnimatedSelector: "popup_is-animated",
-  cardImageSelector: ".card__image",
-  popupTypeImageSelector: ".popup_type_image",
-  cardTemplateSelector: "#card-template",
-  placesListSelector: ".places__list",
-  cardSelector: ".card",
-  cardTitleSelector: ".card__title",
-  cardDeleteButtonSelector: ".card__delete-button",
-  cardLikeButtonSelector: ".card__like-button",
-  popupImageSelector: ".popup__image",
-  popupCaptionSelector: ".popup__caption",
-  cardLikeButtonIsActiveSelector: "card__like-button_is-active",
+  popupIsAnimated: "popup_is-animated",
+};
+
+const cardsPlace = document.querySelector(elementSelectors.placesList);
+const cardTemplate = document.querySelector(
+  elementSelectors.cardTemplate,
+).content;
+const popupButtonCloses = document.querySelectorAll(
+  elementSelectors.popupClose,
+);
+const popupAddNewCard = document.querySelector(
+  elementSelectors.popupTypeNewCard,
+);
+const editButton = document.querySelector(elementSelectors.profileEditButton);
+const addButton = document.querySelector(elementSelectors.profileAddButton);
+const popupEditProfile = document.querySelector(elementSelectors.popupTypeEdit);
+const popupImage = document.querySelector(elementSelectors.popupTypeImage);
+const formEditProfile = document.querySelector(
+  elementSelectors.popupFormEditProfile,
+);
+const formAddCard = document.querySelector(elementSelectors.popupFormNewPlace);
+const profileTitle = document.querySelector(elementSelectors.profileTitle);
+const profileDescription = document.querySelector(
+  elementSelectors.profileDescription,
+);
+
+const openPopupImage = openWindow(popupImage);
+
+function handleClickImage(data) {
+  return (evt) => {
+    document.querySelector(elementSelectors.popupImage).src = data.link;
+    document.querySelector(elementSelectors.popupCaption).textContent =
+      data.name;
+    openPopupImage(evt);
+  };
+}
+
+function addListenersForCard(data) {
+  const template = cardTemplate.querySelector(elementSelectors.card);
+  const card = createCard(data, {
+    template,
+    titleSelecor: elementSelectors.cardTitle,
+    imageSelector: elementSelectors.cardImage,
+  });
+  card
+    .querySelector(elementSelectors.cardDeleteButton)
+    .addEventListener("click", () => {
+      deleteCard(card);
+    });
+  card
+    .querySelector(elementSelectors.cardLikeButton)
+    .addEventListener("click", () => {
+      likeCard(
+        card.querySelector(elementSelectors.cardLikeButton),
+        elementSelectors.cardLikeButtonIsActive,
+      );
+    });
+
+  card
+    .querySelector(elementSelectors.cardImage)
+    .addEventListener("click", handleClickImage(data));
+
+  return card;
+}
+
+function initCards() {
+  cardsPlace.append(...initialCards.map((item) => addListenersForCard(item)));
+}
+
+function initPopup() {
+  popupAddNewCard.classList.add(elementSelectors.popupIsAnimated);
+  popupEditProfile.classList.add(elementSelectors.popupIsAnimated);
+  popupImage.classList.add(elementSelectors.popupIsAnimated);
+  addButton.addEventListener("click", openWindow(popupAddNewCard));
+  editButton.addEventListener("click", (evt) => {
+    // Находим поля формы в DOM;
+    const nameInput = document.querySelector(
+      elementSelectors.popupInputTypeName,
+    ); // Воспользуйтесь инструментом .querySelector()
+    const jobInput = document.querySelector(
+      elementSelectors.popupInputTypeDescription,
+    ); // Воспользуйтесь инструментом .querySelector()
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileDescription.textContent;
+    openWindow(popupEditProfile)(evt);
+  });
+  // Прикрепляем обработчик к форме:
+  // он будет следить за событием “submit” - «отправка»
+  formEditProfile.addEventListener("submit", handleFormEditProfile);
+  formAddCard.addEventListener("submit", handleFormAddCard);
+
+  popupButtonCloses.forEach((element) => {
+    element.addEventListener("click", handlerClosePopup);
+  });
+
+  const cardImages = document.querySelectorAll(elementSelectors.cardImage);
+
+  cardImages.forEach((element) => {
+    element.addEventListener("click", openWindow(popupImage));
+  });
+
+  popupButtonCloses.forEach((element) => {
+    element.addEventListener("click", handlerClosePopup);
+  });
+}
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleFormEditProfile(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  // Так мы можем определить свою логику отправки.
+  // О том, как это делать, расскажем позже.
+  // Получите значение полей jobInput и nameInput из свойства value
+  // Находим поля формы в DOM;
+  const nameInput = document.querySelector(elementSelectors.popupInputTypeName); // Воспользуйтесь инструментом .querySelector()
+  const jobInput = document.querySelector(
+    elementSelectors.popupInputTypeDescription,
+  ); // Воспользуйтесь инструментом .querySelector()
+  const profileJobValue = jobInput.value;
+  const profileNameValue = nameInput.value;
+
+  // Выберите элементы, куда должны быть вставлены значения полей
+  // Вставьте новые значения с помощью textContent
+  profileTitle.textContent = profileNameValue;
+  profileDescription.textContent = profileJobValue;
+
+  closePopup(evt.target.closest(elementSelectors.popup));
+}
+
+function handleFormAddCard(evt) {
+  evt.preventDefault();
+  const cardNameInput = document.querySelector(
+    elementSelectors.popupInputTypeCardName,
+  );
+  const urlInput = document.querySelector(elementSelectors.popupInputTypeUrl);
+
+  const newCard = addListenersForCard({
+    name: cardNameInput.value,
+    link: urlInput.value,
+  });
+  cardsPlace.prepend(newCard);
+
+  cardNameInput.value = "";
+  urlInput.value = "";
+  closePopup(evt.target.closest(elementSelectors.popup));
 }
 
 document.querySelector(".footer__copyright").innerText =
