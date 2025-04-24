83931ae39a7b0f493bff105f11b0ff69e5309599
Andrey Zapozdaev <andrejzapozdaev@gmail.com>
2025-04-24 18:50:50 +0300

    Is very biggest update

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
+
 https://github.com/andreyBodrey2012/mesto-project-ff
--- src/index.js
+++ src/index.js
@@ -1,33 +1,43 @@
-import { initialCards } from "./cards";
-import './css/index.css';
+import { initCards } from "./cards/card";
+import { initPopup } from "./popup/modal";
+import "./css/index.css";
 
-// @todo: Темплейт карточки
-const cardTemplate = document.querySelector('#card-template').content;
-
-// @todo: DOM узлы
-const cardsPlace = document.querySelector('.places__list');
-
-// @todo: Функция создания карточки
-function createCard(params, deleteCard) {
-    const cloneCard = cardTemplate.querySelector('.card').cloneNode(true);
-    cloneCard.querySelector('.card__title').innerText = params.name;
-    const cardImage = cloneCard.querySelector('.card__image');
-    cardImage.src = params.link;
-    cardImage.alt = params.name;
-    cloneCard.querySelector('.card__delete-button').addEventListener('click', () => { deleteCard(cloneCard) });
-
-    return cloneCard;
+export let defautParams = {
+  profileAddButtonSelector: ".profile__add-button",
+  popupTypeNewCardSelector: ".popup_type_new-card",
+  profileEditButtonSelector: ".profile__edit-button",
+  popupTypeEditSelector: ".popup_type_edit",
+  popupCloseSelector: ".popup__close",
+  popupIsOpenedSelector: ".popup_is-opened",
+  popupFormEditProfileSelector: ".popup__form[name='edit-profile']",
+  popupFormNewPlaceSelector: ".popup__form[name='new-place']",
+  popupInputTypeNameSelector: ".popup__input_type_name",
+  popupInputTypeDescriptionSelector: ".popup__input_type_description",
+  profileTitleSelector: ".profile__title",
+  profileDescriptionSelector: ".profile__description",
+  popupInputTypeCardNameSelector: ".popup__input_type_card-name",
+  popupInputTypeUrlSelector: ".popup__input_type_url",
+  popupSelector: ".popup",
+  popupOpened: "popup_is-opened",
+  popupIsAnimatedSelector: "popup_is-animated",
+  cardImageSelector: ".card__image",
+  popupTypeImageSelector: ".popup_type_image",
+  cardTemplateSelector: "#card-template",
+  placesListSelector: ".places__list",
+  cardSelector: ".card",
+  cardTitleSelector: ".card__title",
+  cardDeleteButtonSelector: ".card__delete-button",
+  cardLikeButtonSelector: ".card__like-button",
+  popupImageSelector: ".popup__image",
+  popupCaptionSelector: ".popup__caption",
+  cardLikeButtonIsActiveSelector: "card__like-button_is-active",
 }
 
-// @todo: Функция удаления карточки
-function deleteCard(element) {
-    if (element && typeof element.remove === "function") {
-        element.remove();
-    }
-}
+document.querySelector(".footer__copyright").innerText =
+  `© ${new Date().getFullYear()} Mesto Russia`;
 
 // @todo: Вывести карточки на страницу
-document.querySelector(".footer__copyright").innerText = `© ${(new Date).getFullYear()} Mesto Russia`;
+initCards();
 
-const cards = initialCards.map((item) => createCard(item, deleteCard));
-cardsPlace.append(...cards);
+// @todo: Функция открытия модального окна
+initPopup();
